72172ec046e3d5158225d46914a893777cade47a
Cristopher <cristofer.torres.ct@gmail.com>
2025-04-18 18:57:52 +0200

    hagoCambiosENRama

--- src/main/java/com/example/pruebamongodbcss/AppChatMain.java
+++ src/main/java/com/example/pruebamongodbcss/AppChatMain.java
@@ -6,31 +6,24 @@ import com.mongodb.client.MongoDatabase;
 
 import Utilidades.GestorConexion;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class AppChatMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        // Crear un contenedor para el mensaje
-        StackPane root = new StackPane();
-        
-        // Crear un Label con el texto "hola"
-        Label mensaje = new Label("hola");
-        mensaje.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        
-        // Añadir el Label al contenedor
-        root.getChildren().add(mensaje);
-        
-        // Crear la escena con el contenedor
-        Scene scene = new Scene(root, 320, 240);
-        scene.getStylesheets().add(getClass().getResource("/Estilos/chatOscuro.css").toExternalForm());
+        FXMLLoader fxmlLoader = new FXMLLoader(AppChatMain.class.getResource("/com/example/pruebamongodbcss/panelInicio.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(getClass().getResource("/com/example/pruebamongodbcss/panelInicio.fxml").toExternalForm());
         stage.setTitle("App1!");
-        
+
+
         stage.setScene(scene);
+
+
+
         stage.show();
     }
 
@@ -43,5 +36,7 @@ public class AppChatMain extends Application {
         }catch (Exception ex){
             System.out.println("Error");
         }
+
+
     }
 }
