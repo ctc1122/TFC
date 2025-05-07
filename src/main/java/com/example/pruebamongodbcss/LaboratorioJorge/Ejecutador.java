89e0a1abfeb8de22b90332a647ba75526659e048
ctc1122 <58024954+ctc1122@users.noreply.github.com>
2025-05-07 19:29:36 +0200

    Cambio Laboratorio Jorge

--- src/main/java/com/example/pruebamongodbcss/LaboratorioJorge/Ejecutador.java
+++ src/main/java/com/example/pruebamongodbcss/LaboratorioJorge/Ejecutador.java
@@ -1,4 +1,4 @@
-package com.example.pruebamongodbcss;
+package com.example.pruebamongodbcss.LaboratorioJorge;
 
 import java.io.IOException;
 
@@ -6,16 +6,15 @@ import com.mongodb.client.MongoDatabase;
 
 import Utilidades.GestorConexion;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class PanelInicioMainDiseñoJorge extends Application {
+public class Ejecutador extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(PanelInicioMain.class.getResource("/com/example/pruebamongodbcss/panelInicio.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Ejecutador.class.getResource("/com/example/pruebamongodbcss/panelInicio.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         scene.getStylesheets().add(getClass().getResource("/com/example/pruebamongodbcss/app.css").toExternalForm());
         stage.setTitle("App1!");
@@ -23,11 +22,7 @@ public class PanelInicioMainDiseñoJorge extends Application {
 
         stage.setScene(scene);
 
-        // Cierre seguro de la aplicación
-        stage.setOnCloseRequest(event -> {
-            Platform.exit();
-            System.exit(0);
-        });
+
 
         stage.show();
     }
