f1c275e3485d017d3362321d7d029b696d832c92
Cristopher <cristofer.torres.ct@gmail.com>
2025-05-30 12:54:22 +0200

    CAMBISO ESTRUCTURA PROGRAMA

--- docker/create_user.sql
+++
@@ -1,8 +0,0 @@
--- Script para crear un usuario con todos los privilegios
-CREATE USER IF NOT EXISTS 'tfc'@'%' IDENTIFIED BY 'miclave';
-
--- Otorgar todos los privilegios al usuario
-GRANT ALL PRIVILEGES ON *.* TO 'tfc'@'%' WITH GRANT OPTION;
-
--- Refrescar los privilegios
-FLUSH PRIVILEGES; 
